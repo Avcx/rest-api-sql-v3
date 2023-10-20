37117d0194365e0687ee98bd5afad56c24804831
Tonio Adams <avcx12@gmail.com>
2023-10-20 16:49:54 -0600

    Inital Commit (Models & Installation completed)

---
+++ models/course.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  class Course extends Model {}
+  Course.init(
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      estimatedTime: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      materialsNeeded: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { sequelize }
+  );
+  Course.associate = (models) => {
+    Course.belongsTo(models.User, {
+      foreignKey: {
+        fieldName: "userId",
+        allowNull: false,
+      },
+    });
+  };
+  return Course;
+};
---
+++ models/user.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  class User extends Model {}
+  User.init(
+    {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      emailAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { sequelize }
+  );
+  User.associate = (models) => {
+    User.hasMany(models.Course, {
+      foreignKey: {
+        fieldName: "userId",
+        allowNull: false,
+      },
+    });
+  };
+  return User;
+};
