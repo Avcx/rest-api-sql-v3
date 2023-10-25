47034d0448b7509452cb6d616ae801d5ea7fdcd3
Tonio Adams <avcx12@gmail.com>
2023-10-24 21:53:13 -0600

    user routes basic functionality completed

--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,26 @@ const { authenticateUser } = require("../middleware/userAuth");
 
 router.get(
   "/",
+  authenticateUser,
   asyncHandler(async (req, res, next) => {
-    const users = await User.findAll();
-    res.json([users]);
+    const user = req.currentUser;
+    res.json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      emailAddress: user.emailAddress,
+    });
   })
 );
 
 router.post(
   "/",
-  asyncHandler(async (req, res, next) => {})
+  asyncHandler(async (req, res, next) => {
+    try {
+      User.create(req.body);
+      res.status(201).end();
+    } catch (error) {}
+  })
 );
 
 module.exports = router;
